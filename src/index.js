436165ba4c223a5eba746eb079fd9486ac3ecfee
shushovitJR <ranashushovit@gmail.com>
2024-08-28 21:30:01 +0545

    Backend

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require("path");
+const bcrypt = require("bcrypt");
+
+
+const app = express();
+
+
+app.set('view engine','ejs');
+
+app.get("/",(req,res)=>{
+    res.render("index");
+})
+
+app.get("/",(req,res)=>{
+    res.render("signup");
+})
+
+app.listen(5000,()=>{
+    console.log(`Server running on port 5000`);
+})
